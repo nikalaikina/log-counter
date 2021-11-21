6736267d0aca8e2db0357138c974da21bf313309
Vera Nikalaikina <vnikalaikina@evolutiongaming.com>
2021-11-21 17:57:13 +0300

    IOApp

--- src/main/scala/com/github/nikalaikina/coral/Boot.scala
+++ src/main/scala/com/github/nikalaikina/coral/Boot.scala
@@ -1,15 +1,26 @@
 package com.github.nikalaikina.coral
 
+import cats.effect.{ExitCode, IO, IOApp, Resource, ResourceIO}
 import com.github.nikalaikina.coral.config.AppConfig
 import pureconfig.ConfigSource
 import pureconfig.generic.auto._
-import java.io.{BufferedReader, InputStreamReader}
 
-object Boot extends App {
-  ConfigSource.default.load[AppConfig].foreach { config =>
-    val p = Runtime.getRuntime.exec(config.processCommand)
-    val input = new BufferedReader(new InputStreamReader(p.getInputStream))
-    println(input.readLine)
-    input.close()
+import java.io.{BufferedReader, InputStream, InputStreamReader}
+
+object Boot extends IOApp {
+
+  def input(stream: InputStream): Resource[IO, BufferedReader] = {
+    Resource.make(IO.delay(new BufferedReader(new InputStreamReader(stream))))(r =>
+      IO.delay(r.close())
+    )
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    for {
+      config <- IO.delay(ConfigSource.default.loadOrThrow[AppConfig])
+      process <- IO.delay(Runtime.getRuntime.exec(config.processCommand))
+      input <- input(process.getInputStream).use(i => IO.delay(i.readLine()))
+      _ <- IO.delay(println(input))
+    } yield ExitCode.Success
   }
 }
